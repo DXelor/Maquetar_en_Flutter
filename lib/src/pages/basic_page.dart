c2cfd2851ef8173a893923c4ab1cbd72573ef0aa
DXelor <dzaliaspro@gmail.com>
2020-12-21 18:46:19 -0500

    titulo y subtitulo de la imagen centrados

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 
 class BasicPage extends StatelessWidget {
-  const BasicPage({Key key}) : super(key: key);
+  final estiloTitulo = TextStyle(fontSize: 19.0, fontWeight: FontWeight.bold);
+  final estiloSubTitulo = TextStyle(fontSize: 17.0, color: Colors.grey);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Pagina Basica'),
-      ),
-    );
+        body: Column(
+      children: [
+        Image(
+            image: NetworkImage(
+                'https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg')),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 20.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('PurpurinaStars', style: estiloTitulo),
+                    SizedBox(height: 6.5),
+                    Text('atardecer con cielo purpura', style: estiloSubTitulo),
+                  ],
+                ),
+              ),
+              Icon(Icons.star, color: Colors.amber, size: 25.0),
+              Text(
+                '41',
+                style: TextStyle(fontSize: 18.0),
+              )
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
